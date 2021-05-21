282ecb74566ebc675cbb083aa53f23829466cedb
Aurelien ROY <roy.aurelien@gmail.com>
2021-05-21 12:40:26 +0200

    [IMP] Refactoring

--- analyze.py
+++ analyze.py
@@ -1,103 +1,59 @@
 import os
 import csv
 import networkx as nx
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-from numpy import sqrt
+from pyvis.network import Network
+
+import tools
 
 
-csv_path = './face_reco_results.csv'
-img_path = './known_persons'
+def network(data, img_path=''):
 
-with open(csv_path, 'r') as csvfile:
-    reader = csv.DictReader(csvfile)
-    lines = [row for row in reader if row['name']]
-    
-names = list(set([line['name'] for line in lines]))
-group_by_name = [list(filter(lambda d: d['name'] == name, lines)) for name in names]
+    net = Network(height='750px', width='100%')
 
+    lines = list(filter(lambda x: x['save'], data))
+    hash_list = list(set([str(e['hash']) for e in lines]))
+    node_list = [e[-3:] for e in hash_list]
+    files_list = []
 
+    if img_path is not None:
+        if not os.path.exists(img_path):
+            raise FileExistsError
 
-for item in group_by_name:
-    images = {}
-    name = item[0]['name']
-    nb_lines = len(item)
-    if nb_lines == 1:
-        continue
-        
-    lines = list(filter(lambda x: eval(x['save']) == True, item))
-    first_elem = list(filter(lambda x: eval(x['new_person']) == True and not x['match_hash'], lines))[0]
-#     lines = list(filter(lambda x: x['hash'] != first_elem['hash'], lines))
-    
-    hash_list = [e['hash'] for e in lines if e['match_hash'] or (not e['match_hash'] and eval(e['new_person']))]
-    first_hash = first_elem['hash'][-3:]
-    first_person = first_elem['name']
-    
-    for prefix in hash_list:
-        path = os.path.join(img_path, name, "{}..jpg".format(prefix))
-        e = first_person if prefix == first_hash else prefix
-        images[e] = mpimg.imread(path)
-    
+        for prefix,node in zip(hash_list, node_list):
+            path = os.path.join(img_path, "{}.png".format(prefix))
+            f = path if os.path.isfile(path) else None
+            files_list.append(f)
 
-    
-    G = nx.Graph()
-    G.add_node(first_person, image=images.get(first_hash, None))
-    
-    for elem in hash_list:
-        if elem == first_hash:
-            continue
-        G.add_node(elem[-3:], image=images.get(elem, None))
-        
-    
+    for node,path in zip(node_list, files_list):
+        options = dict(image=path, shape='image') if img_path else dict()
+        net.add_node(node, label=node, **options)
+
     for elem in lines:
         message = {'match':elem['match_percent']}
-        weight = float(elem['match_percent'])/100
-        
+        match = float(elem['match_percent'])
+        distance = float(elem['distance'])
+        value = match / 100
+        # more = elem['save']
+
         if not elem['match_hash']:
             continue
-        
-        a = elem['hash'][-3:] if elem['hash'][-3:] != first_hash else first_person 
-        b = elem['match_hash'][-3:] if elem['match_hash'][-3:] != first_hash else first_person
-        
-        G.add_edge(a, b, weight=weight, message=message)
-    
-#     print(name, len(lines))
-    
-    # print edges with message subject
-    for (u, v, d) in G.edges(data=True):
-        print(f"From: {u} To: {v} Subject: {d['message']['match']}")
-    
-    N = len(lines)
-    pos = nx.spring_layout(G,k=3/sqrt(N))
-#     nx.draw(G, pos, node_size=500, alpha=0.4, edge_color="r", font_size=16, with_labels=True)
-#     nx.draw_networkx(G, pos, width=3, edge_color="r", alpha=0.6) 
-    
+
+        if elem['hash'] == elem['match_hash']:
+            continue
+
+        a = str(elem['hash'])[-3:]
+        b = str(elem['match_hash'])[-3:]
+
+        net.add_edge(a, b, value=value, title="{:.2f}%".format(match))
+
+    return net
 
 
-            
-    fig=plt.figure(figsize=(10,10))
-    ax=plt.subplot(111)
-    ax.set_aspect('equal')
-    nx.draw_networkx(G,pos,width=3, edge_color="r", alpha=0.6, with_labels=False, ax=ax)
+if __name__ == '__main__':
+    csv_path = './results/results.csv'
+    img_path = './results/thumbnails'
 
-    plt.xlim(-1.5,1.5)
-    plt.ylim(-1.5,1.5)
+    lines = tools.read_csv(csv_path, filter_key='hash')
 
-    trans=ax.transData.transform
-    trans2=fig.transFigure.inverted().transform
-
-    piesize=0.05 # this is the image size
-    p2=piesize/2.0
-    for n in G:
-        img = G.nodes[n].get('image', None)
-        if img is None:
-            continue        
-        
-        xx,yy=trans(pos[n]) # figure coordinates
-        xa,ya=trans2((xx,yy)) # axes coordinates
-        a = plt.axes([xa-p2,ya-p2, piesize, piesize])
-        a.set_aspect('equal')
-        a.imshow(img)
-        a.axis('off')
-    ax.axis('off')
-    plt.show()
+    graph = network(lines, img_path=img_path)
+    graph.show('mygraph.html')
--- tools.py
+++ tools.py
@@ -1,3 +1,4 @@
+import csv
 import os
 import uuid
 import numpy as np
@@ -16,40 +17,8 @@ def scan_folder(path, extension=[]):
     files = []
     for root, subdirs, current_files in os.walk(path):
         files += [os.path.join(root,name) for name in current_files if name.endswith(tuple(extension))]
-    
+
     return files
-
-def load_known_encodings(path):
-    face_encodings, names, hash_list = [], [], []
-    numpy_files = scan_folder(path, ['.npy'])
-    
-    for f in numpy_files:
-        array = np.load(f)
-        if array is not None:
-            face_encodings.append(array)
-            names.append(f)
-            hash_list.append(hash_array(array))
-            
-    return names, hash_list, face_encodings
-
-
-def load_from_subdirs(path):
-    extension = ['.npy']
-    face_encodings, names, hash_list = [], [], []
-
-    for root, subdirs, current_files in os.walk(path):
-        for filename in current_files:
-            if not filename.endswith(tuple(extension)):
-                continue
-                
-            array = np.load(os.path.join(root, filename))
-            name = root.replace(path, '').replace('.npy','').replace('/','')
-            if array is not None:
-                face_encodings.append(array)
-                names.append(name)
-                hash_list.append(hash_array(array))
-            
-    return names, hash_list, face_encodings
 
 def resize(picture, max_width=1600):
     #Scale down image if large, performance suggestion snippet from face_recognition
@@ -57,27 +26,26 @@ def resize(picture, max_width=1600):
         scale_factor = float(max_width) / picture.shape[1]
         im = Image.fromarray(picture)
         size = tuple((np.array(im.size) * scale_factor).astype(int))
-        picture = np.array(im.resize(size))        
-        
-#         _logger.info("\tResizing.")
-        _logger.info("\tResizing to {size[0]}x{size[1]} pixels with {o:.2f}x".format(size=size, o=scale_factor))    
-        
+        picture = np.array(im.resize(size))
+
+        _logger.debug("\tResizing to {size[0]}x{size[1]} pixels with {o:.2f}x".format(size=size, o=scale_factor))
+
     return picture
 
 def new_person(path, prefix='person'):
     if not os.path.isdir(path):
         os.mkdir(path)
-        
+
     dirs = os.listdir(path)
     if dirs:
         index = sorted([int(elem.split('_')[1]) for elem in dirs])[-1]
         index += 1
     else:
         index = 1
-        
+
     name = "_".join([prefix,str(index)])
     os.mkdir(os.path.join(path,name))
-    
+
     return name
 
 def generate_id(**kwargs):
@@ -86,7 +54,7 @@ def generate_id(**kwargs):
 def save_array(path, filename, array):
     if not os.path.isdir(path):
         os.mkdir(path)
-        
+
     filepath = os.path.join(path, "{}.npy".format(filename))
     encoded_file = np.save(filepath, array)
 
@@ -101,59 +69,95 @@ def mask_circle_transparent(pil_img, blur_radius, offset=0):
     result.putalpha(mask)
     result = result.resize((64,64), Image.LANCZOS)
 
-    return result    
-    
+    return result
+
 def save_thumbnail(path, filename, image, pos=(), extension='jpg', padding=0, crop='square'):
     if not os.path.isdir(path):
         os.mkdir(path)
-        
+
     filepath = os.path.join(path, "{}.{}".format(filename, extension))
-    
+
     if padding:
 #         pos = (left, top, right, bottom)
         pos = list(pos)
-        pos[0] -= padding 
+        pos[0] -= padding
         pos[1] -= padding
         pos[2] += padding
         pos[3] += padding
         pos = tuple(pos)
-        
+
     new_image = image.crop(pos)
-    
+
     if crop == 'circle':
         new_image = mask_circle_transparent(new_image, 0)
-    
+
     new_image.save(filepath, quality=90)
-    
-def compress_and_save(path, list_of_names, list_of_array):
-    data = {name:array for array, name in zip(list_of_array, list_of_names)}
+
+def compress_and_save(path, list_of_key, list_of_array, delete=False):
+    data = {key:array for key, array in zip(list_of_key, list_of_array)}
     np.savez_compressed(path, **data)
-    
+
 def load_from_npz(path):
     if not os.path.isfile(path):
+        _logger.warning('File not found: {}'.format(path))
         return [], [], []
-    
-    tmp = np.load(path)
-    hash_list = [hash_array(array) for array in tmp.values()]
-    return list(tmp.keys()), hash_list, list(tmp.values())
 
-def load_known_encodings(npz_file, npy_folder=None):
-    names, hash_list, encodings = load_from_npz(npz_file)
-    
-    if npy_folder and not names or not encodings:
-        _logger.debug("<<Switch back to npy files>>")
-        names, hash_list, encodings = load_from_subdirs(npy_folder)
-    
+    tmp = np.load(path)
+    # hash_list = [hash_array(array) for array in tmp.values()]
+    # return list(tmp.keys()), hash_list, list(tmp.values())
+
+    return [], [int(k) for k in tmp.keys()], list(tmp.values())
+
+def load_encodings(path, npz_file=None):
+    if npz_file is None:
+        # try to load npi files from path
+        raise NotImplementedError
+    else:
+        filepath = os.path.join(path, npz_file)
+        names, hash_list, encodings = load_from_npz(filepath)
+
     return names, hash_list, encodings
 
-
-
-def get_equation(series):
+def linear_equation(series):
     x = [serie[0] for serie in series]
     y = [serie[1] for serie in series]
     min_value, max_value = min(x),max(x)
-    
+
     return Polynomial.fit(x, y, 1, window=[min_value,max_value])
-    
+
 #     x = [0.1, 0.55]
 #     y = [90, 50]
+
+def polynomial_equation(series):
+    x = [serie[0] for serie in series]
+    y = [serie[1] for serie in series]
+    min_value, max_value = min(x),max(x)
+
+    return Polynomial.fit(x, y, 1, window=[min_value,max_value])
+
+def read_csv(path, filter_key=None):
+    with open(path, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        if filter_key:
+            lines = [row for row in reader if row[filter_key]]
+        else:
+            lines = [row for row in reader]
+
+    for line in lines:
+        if 'save' in line:
+            line['save'] = eval(line['save'])
+
+    return lines
+
+def save_csv(path, fieldnames, lines, append=True):
+    create_file = True if not os.path.isfile(path) else False
+    mode = 'a' if append else 'w'
+
+    with open(path, mode) as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        if create_file:
+            writer.writeheader()
+
+        for line in lines:
+            writer.writerow(line)
